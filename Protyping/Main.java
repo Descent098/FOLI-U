f13ce842723306c0d9071e31ea13e2bdf77e6796
descent098 <kieranw098@gmail.com>
2018-02-07 10:08:42 -0700

    Updated Main: now has a choice menu

--- Protyping/Main.java
+++ Protyping/Main.java
@@ -10,15 +10,7 @@ public class Main{
     // int choice =  mainclassinput.nextInt();
     // mainclassinput.close();
     //need an if statement for the slection made and then import the corresponding class
-    s1.set_User_Attributes();
-    if (s1.GPA == 2){
-      s1.calc_GPA();
-  }
-    if (s1.GPA == 1){
-      Scanner GPAInput = new Scanner(System.in);
-      System.out.println("\nEnter your GPA: ");
-      s1.GPA = GPAInput.nextDouble();
-    }
+    selectMenu();
     print_Vars();
 
   }
@@ -31,5 +23,40 @@ public class Main{
     System.out.println("\nUsers Program Year is: "+ s1.program_year);
     System.out.println("\nUsers Degree is: "+ s1.degree);
     System.out.println("\nUsers GPA is: "+ s1.GPA);
+  }
+
+public static void createUser(){
+  /** method to call all methods needed to create a user*/
+  s1.set_User_Attributes();
+
+  if (s1.GPA == 2){
+    s1.calc_GPA();
+}
+
+  if (s1.GPA == 1){
+    Scanner GPAInput = new Scanner(System.in);
+    System.out.println("\nEnter your GPA: ");
+    s1.GPA = GPAInput.nextDouble();
+  }
+}
+
+
+  public static void selectMenu(){
+    /** Gives the user options to select from that call methods*/
+    Scanner selection = new Scanner(System.in);
+    System.out.println("\nPlease select an option:");
+    System.out.println("1 for creating a new user:");
+    System.out.println("2 for printing a users attributes:");
+    System.out.println("3 to exit program:");
+    int choice = selection.nextInt();
+    if (choice == 1){
+      createUser();
+    }
+    if (choice == 2){
+      print_Vars();
+    }
+    if (choice == 3){
+      System.exit(0);
+    }
   }
 }
