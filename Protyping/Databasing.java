6af67746cffe3853b7cc5f3a8cc26e54d8db5fd8
Descent098 <kieranw098@gmail.com>
2018-02-13 20:10:13 -0700

    Added ability to specify how many rando's you want in databasing

--- Protyping/Databasing.java
+++ Protyping/Databasing.java
@@ -1,20 +1,26 @@
 import java.util.*;
 
-
+/**class that holds all the database specific methods and initialization*/
 public class Databasing{
 
   public static void main(String[] args) {
   HashMap<String, User> database = new HashMap();
-
-  for(int i=1; i<11; i++){
+  int JohnDoeS = howManyRandos();
+  for(int i=0; i<JohnDoeS; i++){
     JohnDoe temp = newJohnDoe();
     database.put(temp.UID, temp);
   }
   System.out.println(database);
  }
 
+/** Creates a new random User from class JohnDoe*/
 public static JohnDoe newJohnDoe(){
   JohnDoe jd = new JohnDoe();
   return jd;
+  }
+public static int howManyRandos(){
+  Scanner randos = new Scanner(System.in);
+  System.out.println("How Many randos?: ");
+  return randos.nextInt();
 }
 }
