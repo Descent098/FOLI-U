2dddfb98df01be7962f8430658da0ed6e998258f
kieran.wood1 <kieran.wood1@zone47-ea.cs.ucalgary.ca>
2018-02-16 15:54:41 -0700

    rearanged database class

--- Protyping/Database.java
+++ Protyping/Database.java
@@ -4,9 +4,12 @@ import java.util.*;
 /**class that holds all the database specific methods and initialization*/
 public class Database{
 
+  public HashMap<String, User> database = new HashMap();
+
+
   public void createandSearchDatabase() {
   Scanner continuetoSearch = new Scanner(System.in);
-  HashMap<String, User> database = new HashMap();
+
   int JohnDoeS = howManyRandos();
   for(int i=0; i<JohnDoeS; i++){
     JohnDoe temp = newJohnDoe();
@@ -14,23 +17,25 @@ public class Database{
   }
   System.out.println(database);
   while (true){
-  String searchedUser = searchUser();
+    searchUser();
+    if (continuetoSearch.nextInt() == 1){
+      break;
+    }
+}
+ }
+/** A method that takes user input and returns a string to be used to search
+the database for the corresponding user */
+public void searchUser(){
+  Scanner searchUID = new Scanner(System.in);
+
+  System.out.println("\nPlease enter the UID of the user you would like to find: ");
+  String searchedUser = searchUID.nextLine();
   System.out.println("\nDatabase Returned: ");
   database.get(searchedUser).printContactInfo();
   System.out.println("\nWould you like to search again? (1 for no, 2 for yes): ");
-  if (continuetoSearch.nextInt() == 1){
-    break;
-  }
-
 }
- }
 
-public String searchUser(){
-  Scanner searchUID = new Scanner(System.in);
-  System.out.println("\nPlease enter the UID of the user you would like to find: ");
-  return searchUID.nextLine();
 
-}
 /** Creates a new random User from class JohnDoe*/
 public JohnDoe newJohnDoe(){
   JohnDoe JoDo = new JohnDoe();
