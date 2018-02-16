5c30f80c1310c32bb7b6f9ae955decd3d9d92158
descent098 <kieranw098@gmail.com>
2018-02-16 16:40:29 -0700

    Fully randomized jhon doe

--- Protyping/JohnDoe.java
+++ Protyping/JohnDoe.java
@@ -1,35 +1,71 @@
 import java.util.*;
 
 public class JohnDoe extends Student{
-  public int programYear = 4;
-  public String UID = createRandomUIDNumber(); //university ID number
-  public String university = "U of C"; //Create a dropdown list of options
-  public String degree = "CPSC";
-  public double GPA = 0;
+  public int programYear;
+  public String UID; //university ID number
+  public String university; //Create a dropdown list of options
+  public String degree;
+  public double GPA;
 
 
   JohnDoe(){
     randomUser();
+    createRandomUIDNumber();
+    createRandomUniversity();
+    createRandomDegree();
+    createRandomYear();
+    createRandomGPA();
+
   }
   /** A method to create a random UID #*/
-  public String createRandomUIDNumber(){
+  public void createRandomUIDNumber(){
     Random rand = new Random();
 
     int n = (30000000 + rand.nextInt(900000) + 1);
     String randomUID = Integer.toString(n);
-    return randomUID;
+    UID = randomUID;
+  }
+
+  /** A method to create a random University*/
+  public void createRandomUniversity(){
+    Random rand = new Random();
+    String names[] = {"University of Calgary","University of Lethbridge","University of Alabama","University of Alberta","University of Toronto","University of Oregon","University of Portland","University of Staffordshire","University of Westminister","University of Kent","University of California","Massachusetts Institute of Technology","keele university","University of Milan","University of Turin","University of Bologna","Ca' Foscari University of Venice","University of Grenoble","University of British Columbia","Université Pierre et Marie Curie","Harvard University","McGill University","Princeton University","University of Chicago","University of Halifax","Stanford University","University of Pennsylvania","Mount Royal Collage","St. Mary's University", "Devry"};
+    String randomUniversity = (names[(rand.nextInt(29))]);
+    university = randomUniversity;
+  }
+
+  /** A method to create a random Year*/
+  public void createRandomYear(){
+    Random rand = new Random();
+    int n = (rand.nextInt(7));
+    programYear = n;
+  }
+
+  /** A method to create a random GPA #*/
+  public void createRandomGPA(){
+    Random rand = new Random();
+    Double n = (rand.nextDouble());
+    GPA = n;
+  }
+
+  /** A method to create a random University*/
+  public void createRandomDegree(){
+    Random rand = new Random();
+    String names[] = {" CPSC","BIO", "MED", "ENG", "SOCI", "PSYC"};
+    String randomUniversity = (names[(rand.nextInt(5))]);
+    university = randomUniversity;
   }
 
 
-  public void print_Vars(){
+  public void printContactInfo(){
     /** A method that prints a users atributes*/
     System.out.println("\nSearching database for: "+ fullName);
     System.out.println("\n\nUsers Name is: "+ fullName);
-    System.out.println("\nUsers UID is: "+ UID);
-    System.out.println("\nUsers University is: "+ university);
-    System.out.println("\nUsers Program Year is: "+ programYear);
-    System.out.println("\nUsers Degree is: "+ degree);
-    System.out.println("\nUsers GPA is: "+ GPA);
+    System.out.println("Users UID is: "+ UID);
+    System.out.println("Users University is: "+ university);
+    System.out.println("Users Program Year is: "+ programYear);
+    System.out.println("Users Degree is: "+ degree);
+    System.out.println("Users GPA is: "+ GPA);
     System.out.println("Phone Numebr: "+ phoneNumber);
     System.out.println("Email Address: "+ email);
     System.out.println("Country: " + country);
