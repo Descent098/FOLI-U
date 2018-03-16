787fd6f05de89ec56a0749e0294a85be2568a7d5
descent098 <kieranw098@gmail.com>
2018-03-16 17:13:48 -0600

    Added option to choose gui or text version from program

--- Project/application/Main.java
+++ Project/application/Main.java
@@ -16,28 +16,28 @@ public class Main extends Application { //main class
 	private Stage stage;
 	private AnchorPane mainPage;
 	private Scene mainScene;
-	
+
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		this.stage = primaryStage;
-		
+
 		FXMLLoader loader = new FXMLLoader();
 
 		mainPage = loader.load(getClass().getResource("mainpage.fxml")); //connects to mainpage.fxml
 		mainScene = new Scene(mainPage, 375, 559);
-		
+
 		Controller controller = loader.getController();
 
         primaryStage.setTitle("Foli-U"); //title of stage
         primaryStage.setScene(mainScene);
         primaryStage.show(); //displays stage/app
 	}
-	
+
 	public Stage getPrimaryStage() {
 		return stage;
 	}
-	
+
 	public Main() {
 	}
 
