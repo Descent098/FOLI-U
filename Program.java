0b4a1e20174099c760a80ed8e5861e3642d677e2
William Chan <willchan789@gmail.com>
2018-04-11 19:35:00 -0600

    organized program code

--- Program.java
+++ Program.java
@@ -9,34 +9,34 @@ import tests.StudentTest;
 import tests.UserTest;
 import utilities.Menu;
 
-public class Program{
+public class Program {
 
-    public static void main(String[] args) {
-      Scanner GUIorText = new Scanner(System.in);
-      System.out.println("Gui(1), Text Version (2) or Run Tests(3)");
-      int choice = GUIorText.nextInt();
-      String[] q = new String[1];
-      switch(choice){
+public static void main(String[] args) {
+        Scanner GUIorText = new Scanner(System.in);
+        System.out.println("Gui(1), Text Version (2) or Run Tests(3)");
+        int choice = GUIorText.nextInt();
+        String[] q = new String[1];
+        switch(choice) {
 
-        case 1: while(true){
-          Main.main(q);
+        case 1: while(true) {
+                        Main.main(q);
         }
 
-        case 2:Menu m1 = new Menu();
-          while (true){
-            m1.selectMenu();
-          }
+        case 2: Menu m1 = new Menu();
+                while (true) {
+                        m1.selectMenu();
+                }
 
         case 3: UserTest ut = new UserTest();
-        System.out.println("\nAll User Tests Completed \n\n");
-        StudentTest st = new StudentTest();
-        System.out.println("\nAll Student Tests Completed \n\n");
-        EmployerTest et = new EmployerTest();
-        System.out.println("\nAll Employer Tests Completed \n\n");
-        StatisticsTest stat1 = new StatisticsTest();
-        System.out.println("\nAll Statistics Tests Completed \n\n");
-        System.out.println("\nAll Tests Completed");
-          
-  }
+                System.out.println("\nAll User Tests Completed \n\n");
+                StudentTest st = new StudentTest();
+                System.out.println("\nAll Student Tests Completed \n\n");
+                EmployerTest et = new EmployerTest();
+                System.out.println("\nAll Employer Tests Completed \n\n");
+                StatisticsTest stat1 = new StatisticsTest();
+                System.out.println("\nAll Statistics Tests Completed \n\n");
+                System.out.println("\nAll Tests Completed");
+
+        }
 }
 }
