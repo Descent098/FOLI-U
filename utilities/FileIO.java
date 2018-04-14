0aabea46b468a44c198f42e5378ae7d58141634b
seth239 <seth.campbell2597@Gmail.com>
2018-04-13 19:41:39 -0600

    more tweaks

--- utilities/FileIO.java
+++ utilities/FileIO.java
@@ -7,19 +7,22 @@ import users.User;
 
 
 @SuppressWarnings("unchecked")
-/**Class containing all of the methods for creating and reading files*/
+/**
+  * Class containing all of the methods for creating and reading files. Utilized
+  * by both the Text UI and GUI versions of the application
+  */
 public class FileIO {
 
-/**Method that saves the current database to a file*/
+/** Method that saves the current database to a file
+  * @param db   HashMap of the database
+  */
 public void fileSave(HashMap<String,User> db) {
         Scanner input = new Scanner(System.in);
         File data;
         boolean createOrLoadFile = false;
 
 
-        /*
-           START OF FILE CREATION
-         */
+        // START OF FILE CREATION
         String fileName = "dataFile";
         do {
 
@@ -59,7 +62,9 @@ public void fileSave(HashMap<String,User> db) {
 
 }
 
-/**Method that loads a database from a file*/
+/** Method that loads a database from a file
+  * @return HashMap   hashmap read from a file
+  */
 public HashMap<String, User> fileLoad() {
         Scanner input = new Scanner(System.in);
         File data;
@@ -90,13 +95,16 @@ public HashMap<String, User> fileLoad() {
                                 System.out.println("Class could not be used to cast object.");
                                 System.err.println("ClassNotFoundException: " + e.getMessage());
                         }
-                } //NEED TO THROW AN ERROR IF FILE DOES NOT EXIST
+                } 
 
         } while (createOrLoadFile != true);
         return null;
 }
 
 
+/** Checks whether dataFile.dat exists or not
+  * @return boolean   if it exists or not
+  */
 public boolean exitCheck() {
         Scanner input = new Scanner(System.in);
         File data;
